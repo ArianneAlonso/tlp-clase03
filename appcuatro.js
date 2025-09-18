c0b44c31cb4c526d70f926f7d8b43ffad1b595c1
ArianneALonso <ariannealonso8@gmail.com>
2025-09-18 15:40:22 -0300

    actualizacion del tp

--- appcuatro.js
+++ appcuatro.js
@@ -1,1 +1,9 @@
-cuentabancaria
+const toUpperCaseOriginal = String.prototype.toUpperCase;
+
+String.prototype.toUpperCase = function(){
+    return "ESTO ESTÁ PROHIBIDO";
+
+};
+
+const palabra ="esto no esta prohibido";
+console.log(palabra.toUpperCase());
--- apptres.js
+++ apptres.js
@@ -1,1 +1,9 @@
-CuentaBancaria
+String.prototype.concatenarPalabra = function(palabra){
+    return `${this} ${palabra}`;
+
+};
+let palabrauno = "palabra";
+let palabrados = palabra1.concatenarPalabra("concantenada");
+
+console.log(palabrauno);
+console.log(palabrados);
